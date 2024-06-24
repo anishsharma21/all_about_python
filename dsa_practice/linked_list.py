4075314b043edccaf103e019f62ad912a92be228
anishsharma21 <anisharma2103@gmail.com>
2024-06-24 19:15:54 +1000

    - lots more methods on the singly linked list implemented, most notably the reverse a linked list method which I managed to get with recursion (2 implementations)

--- dsa_practice/linked_list.py
+++ dsa_practice/linked_list.py
@@ -13,6 +13,7 @@ class SinglyLinkedList:
     
     def is_empty(self):
         if not self.head:
+            print("\nlist is empty...\n")
             return True
         return False
     
@@ -28,6 +29,8 @@ class SinglyLinkedList:
         return new_node
             
     def traverse(self):
+        if self.is_empty():
+            return
         cur_node = self.head
         while cur_node:
             print(str(cur_node.value), end='')
@@ -38,7 +41,6 @@ class SinglyLinkedList:
     
     def insert_after_value(self, target, value):
         if self.is_empty():
-            print("list is empty...\n")
             return
         cur_node = self.head
         while cur_node:
@@ -48,11 +50,10 @@ class SinglyLinkedList:
                 cur_node.next = new_node
                 return new_node
             cur_node = cur_node.next
-        print(f"target value, '{target}', not found...\n")
+        print(f"\ntarget value, '{target}', not found...\n")
     
     def delete_by_value(self, target):
         if self.is_empty():
-            print("list is empty...\n")
             return
         if self.head.value == target:
             self.head = self.head.next
@@ -63,20 +64,142 @@ class SinglyLinkedList:
                 prev_node.next = prev_node.next.next
                 return
             prev_node = prev_node.next
-        print(f"target value, '{target}', not found...\n")
+        print(f"\ntarget value, '{target}', not found...\n")
+    
+    def delete_by_position(self, position):
+        """Position is 1-indexed. Deletes a node at the given position."""
+        if position < 1:
+            print("Invalid position. Position should be >= 1.")
+            return
+        if self.is_empty():
+            return
+        if position == 1:
+            self.head = self.head.next
+            return
+        cur_node = self.head
+        track = 2
+        while cur_node.next:
+            if track == position:
+                cur_node.next = cur_node.next.next
+                return
+            cur_node = cur_node.next
+            track += 1
+        print(f"\ntarget position, '{position}', is out of bounds...\n")
+    
+    def delete_head(self):
+        if self.is_empty():
+            return
+        self.head = self.head.next
+    
+    def delete_last_node(self):
+        if self.is_empty() or not self.head.next:
+            self.head = None
+            return
+        prev_node = self.head
+        while prev_node.next.next:
+            prev_node = prev_node.next
+        prev_node.next = None
+    
+    def search(self, target):
+        if self.is_empty():
+            return
+        cur_node = self.head
+        while cur_node:
+            if cur_node.value == target:
+                return cur_node
+            cur_node = cur_node.next
+        print("\ntarget node not found...\n")
+        return None
 
-node1 = Node(5)
-sll = SinglyLinkedList(node1)
-sll.traverse()
-sll.set_head(4)
-sll.traverse()
-appended_node = sll.append(2)
-sll.traverse()
-sll.set_head(1)
-sll.traverse()
-sll.insert_after_value(4, 9)
-sll.traverse()
-sll.insert_after_value(2, 13)
-sll.traverse()
-sll.delete_by_value(1)
-sll.traverse()
+    def length(self):
+        if self.is_empty():
+            return 0
+        length = 0
+        cur_node = self.head
+        while cur_node:
+            length += 1
+            cur_node = cur_node.next
+        return length
+
+    def reverse(self, node=None):
+        if self.is_empty():
+            return
+        if node == None:
+            node = self.head
+        if node.next == None:
+            self.head = node
+            return node
+        next_node = self.reverse(node.next)
+        next_node.next = node
+        node.next = None
+        return node
+    
+    def another_reverse(self, node=None):
+        if node is None:
+            if self.is_empty():
+                return
+            node = self.head
+        if node.next is None:
+            self.head = None
+            return node
+        next_node = self.reverse(node.next)
+        node.next.next = node
+        node.next = None
+        return next_node
+
+# Create a linked list with values 1 -> 2 -> 3
+linked_list = SinglyLinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+
+# Print the original list
+print("Original list:")
+linked_list.traverse()
+
+# Reverse the list
+linked_list.another_reverse()
+
+# Print the reversed list
+print("Reversed list:")
+linked_list.traverse()    
+
+# node1 = Node(5)
+# sll = SinglyLinkedList(node1)
+# sll.traverse()
+# sll.set_head(4)
+# sll.traverse()
+# appended_node = sll.append(2)
+# sll.traverse()
+# sll.set_head(1)
+# sll.traverse()
+# sll.insert_after_value(4, 9)
+# sll.traverse()
+# sll.insert_after_value(2, 13)
+# sll.traverse()
+# sll.delete_by_value(1)
+# sll.traverse()
+# print("deleting the second value")
+# sll.delete_by_position(2)
+# sll.traverse()
+# print("deleting the last value")
+# sll.delete_by_position(4)
+# sll.traverse()
+# sll.set_head(5)
+# sll.set_head(1)
+# sll.traverse()
+# sll.delete_head()
+# sll.traverse()
+# sll.delete_last_node()
+# sll.traverse()
+# sll.set_head(2)
+# sll.set_head(9)
+# sll.set_head(5)
+# sll.set_head(1)
+# print("list before reverse:")
+# sll.traverse()
+# print()
+# sll.reverse()
+# print("list after reverse")
+# sll.traverse()
+# print(sll.head.value)
