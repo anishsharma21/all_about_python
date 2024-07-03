27df64c892384bf7c08f391d68564a33c169d9c9
anishsharma21 <anisharma2103@gmail.com>
2024-07-03 18:01:59 +1000

    implemented a DynamicArray class that emulates a simple version of the sequence classes in python and how they dynamically changed

--- python_data_structures_book_exercises/chapter5.py
+++ python_data_structures_book_exercises/chapter5.py
@@ -1,9 +1,41 @@
 import sys
+import ctypes
 
-n = 10
-data = []
-for k in range(n):
-    a = len(data)
-    b = sys.getsizeof(data)
-    print(f"Number of items in list: {a} | Size of list in bytes: {b} bytes")
-    data.append(None)
+def skip():
+    n = 10
+    data = []
+    for k in range(n):
+        a = len(data)
+        b = sys.getsizeof(data)
+        print(f"Number of items in list: {a} | Size of list in bytes: {b} bytes")
+        data.append(None)
+
+class DynamicArray:
+    def __init__(self, capacity=1):
+        self._n = 0
+        self._capacity = capacity
+        self._array = self._make_array(self._capacity)
+    
+    def _make_array(self, capacity):
+        return (capacity * ctypes.py_object)()
+    
+    def __len__(self):
+        return self._n
+    
+    def __getitem__(self, k):
+        if not 0 <= k < self._n:
+            raise IndexError(f"invalid index: {k}")
+        return self._array[k]
+    
+    def append(self, value):
+        if self._n == self._capacity:
+            self.resize(self._capacity * 2)
+        self._array[self._n] = value
+        self._n += 1
+
+    def resize(self, new_capacity):
+        new_arr = self._make_array(new_capacity)
+        for k in range(self._n):
+            new_arr[k] = self._array[k]
+        self._array = new_array
+        self._capacity = new_capacity
