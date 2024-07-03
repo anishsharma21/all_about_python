9c0427e36bd09f0f2efea948adc26a1f7c67271f
anishsharma21 <anisharma2103@gmail.com>
2024-07-03 19:22:45 +1000

    class for scoreboard and game entries to emulate adding and getting items from a dynamic list in a more practical application

--- python_data_structures_book_exercises/chapter5.py
+++ python_data_structures_book_exercises/chapter5.py
@@ -39,3 +39,42 @@ class DynamicArray:
             new_arr[k] = self._array[k]
         self._array = new_array
         self._capacity = new_capacity
+
+class GameEntry:
+    def __init__(self, score, name):
+        self._score = score
+        self._name = name
+    
+    def get_score(self):
+        return self._score
+
+    def get_name(self):
+        return self._name
+    
+    def __str__(self):
+        return f"{self._name}: {self._score}"
+
+class Scoreboard:
+    def __init__(self, capacity=10):
+        self._n = 0
+        self._board = [None] * capacity
+    
+    def __getitem__(self, k):
+        if not 0 <= k < len(self._board):
+            raise IndexError('invalid index')
+        return self._board[k]
+    
+    def __str__(self):
+        return '\n'.join(str(entry) for entry in self._board)
+    
+    def add(self, entry):
+        score = entry.get_score()
+        good = n < len(self._board) or score > self._board[-1].get_score()
+        if good:
+            if self._n < len(self._board):
+                self._n += 1
+            j = self._n - 1
+            while j > 0 and self.board [j-1].get_score() < score:
+                self._board[j] = self._board[j-1]
+                j -= 1
+            self._board[j] = entry
