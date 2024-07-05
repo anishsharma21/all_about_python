bb51d65c7d984ccae696f4837186dce91b6d9987
anishsharma21 <anisharma2103@gmail.com>
2024-07-05 16:24:00 +1000

    working through leetcode problems to improve dsa and problem solving skills, starting simple as usual and working my way up

--- python_data_structures_book_exercises/chapter6.py
+++ python_data_structures_book_exercises/chapter6.py
@@ -65,6 +65,24 @@ def longestCommonPrefix(strs):
 
 print(longestCommonPrefix(['flower', 'flow', 'flight']))
 
+string = 'haystack'
+print(string[0:3])
 
 
+def addBinary(a: str, b: str) -> str:
+    s = []
+    carry = 0
+    i = len(a) - 1
+    j = len(b) - 1
 
+    while i >= 0 or j >= 0 or carry:
+        if i >= 0:
+            carry += int(a[i])
+            i -= 1
+        if j >= 0:
+            carry += int(b[j])
+            j -= 1
+        s.append(str(carry % 2))
+        carry //= 2
+
+    return ''.join(reversed(s))
