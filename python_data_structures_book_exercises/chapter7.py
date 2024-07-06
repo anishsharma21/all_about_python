f221b7910b52b6b31974450dc52b110f73f22ad0
anishsharma21 <anisharma2103@gmail.com>
2024-07-06 21:04:04 +1000

    implemented a _DoublyLinkedBase - underscored because it is a private class, or interface, that will be inherited from by the next data structures I implement using the doubly-linked-list representation where a _prev and _next attribtue exist for easier insertion and deletion. Note also the creation of sentinel header and trailer nodes for making edge cases easier to handle.

--- python_data_structures_book_exercises/chapter7.py
+++ python_data_structures_book_exercises/chapter7.py
@@ -123,4 +123,36 @@ class CircularQueue:
         if self.is_empty():
             raise Empty('queue is empty')
         self.tail = self.tail._next
+
+class _DoublyLinkedBase:
+    class _Node:
+        __slots__ = '_element', '_prev', '_next'
+        def __init__(self, element, prev, next):
+            self._element = element
+            self._prev = prev
+            self._next = next
     
+    def __init__(self):
+        self._header, self._trailer = _Node(None, None, None), _Node(None, None, None)
+        self._header._next, self._trailer._prev= self._trailer, self._header
+        self._size = 0
+    
+    def __len__(self):
+        return self._size
+    
+    def is_empty(self):
+        return self.size == 0
+    
+    def _insert_between(self, e, predecessor, successor):
+        new = _Node(e, predecessor, successor)
+        predecessor._next, successor._prev = new, new
+        self._size += 1
+        return new
+    
+    def _delete_node(self, node):
+        predecessor, successor = node._prev, node._next
+        predecessor._next, successor._prev = successor, predecessor
+        self._size -= 1
+        e = node._element
+        node._prev = node._next = node._element = None
+        return e
