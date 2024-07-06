2e7fd96ebaf9ad5f5219f3cabcf7ba7f643b728a
anishsharma21 <anisharma2103@gmail.com>
2024-07-06 20:23:17 +1000

    implemented a simple LinkedQueue class - next up is a CircularQueue class for situations where enqueueing and dequeing operations can be costly and it is better to use pointers for cycling through a linked list - i.e. for something like a round-robon scheduler

--- python_data_structures_book_exercises/chapter7.py
+++ python_data_structures_book_exercises/chapter7.py
@@ -3,10 +3,10 @@ class Empty(Exception):
 
 class LinkedStack:
     class Node:
-        __slots__ = 'value', 'next'
-        def __init__(self, value, next):
-            self.value = value
-            self.next = next
+        __slots__ = '_value', '_next'
+        def __init__(self, _value, _next):
+            self._value = _value
+            self._next = _next
             
     def __init__(self, head=None):
         self.size = 0
@@ -18,19 +18,61 @@ class LinkedStack:
     def is_empty(self):
         return self.size == 0
     
-    def push(self, value):
-        self.head = self.Node(value, self.head)
+    def push(self, _value):
+        self.head = self.Node(_value, self.head)
         self.size += 1
         
     def top(self):
         if self.is_empty():
             raise Empty('stack is empty')
-        return self.head.value
+        return self.head._value
 
     def pop(self):
         if self.is_empty():
             raise Empty('stack is empty')
-        top = self.head.value
-        self.head = self.head.next
+        top = self.head._value
+        self.head = self.head._next
         self.size -= 1
         return top
+
+class LinkedQueue:
+    class Node:
+        __slots__ = '_value', '_next'
+        def __init__(self, _value, _next):
+            self._value = _value
+            self._next = _next
+    
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+    
+    def __len__(self):
+        return self.size
+    
+    def is_empty(self):
+        return self.size == 0
+    
+    def first(self):
+        if self.is_empty():
+            raise Empty('queue is empty')
+        return self.head._value
+    
+    def dequeue(self):
+        if self.is_empty():
+            raise Empty('queue is empty')
+        first = self.head._value
+        self.head = self.head._next
+        self.size -= 1
+        if self.is_empty():
+            self.tail = None
+        return first
+    
+    def enqueue(self, value):
+        tail = Node(value, None)
+        if self.is_empty():
+            self.head = tail
+        else:
+            self.tail._next = tail
+        self.tail = tail
+        self.size += 1
