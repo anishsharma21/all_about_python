98b5bfc0c994503caa3082ed5ee3aee3f88a4c9f
anishsharma21 <anisharma2103@gmail.com>
2024-07-07 12:38:03 +1000

    took a while but implemented a PositionalList class with a Position class too inside. Really interesting data structure that allows for O(1) operations and addresses problems with traversing and finding elements in a linked list. This also addresses problems with Array based data structures where operations can be expensive for inserting and deleting elements, as well as memory management when the underlying array needs to be dynamically resized - the amortized approach to this improves efficiency but are not good enough for bigger systems. Positional lists are really cool - they make use of Position instances for each node and work as markers/references - then we can just use the inherited _insert_between and _delete_node class methods to perform operations as usual but with greater efficiency - the only problem seems to be the additional memory usage required for these Position instances but it seems as though the tradeoff is optimal when considering the runtime and memory management improvements

--- python_data_structures_book_exercises/chapter7.py
+++ python_data_structures_book_exercises/chapter7.py
@@ -183,4 +183,81 @@ class LinkedDeque(_DoublyLinkedBase):
         if self.is_empty():
             raise Empty('queue is empty')
         return self._delete_node(self._trailer._prev)
+
+class PositionalList(_DoublyLinkedBase):
+    class Position:
+        __slots__ = '_container', '_node'
+        def __init__(self, container, node):
+            self._container = container
+            self._node = node
         
+        def element(self):
+            return self._node._element
+        
+        def __eq__(self, other):
+            return type(self) is type(other) and self._node is other._node
+
+        def __ne__(self, other):
+            return not (self == other)
+    
+    def _validate(self, p):
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be proper Position type')
+        if p._container is not self:
+            raise ValueError('p does not belong to this container')
+        if p._node._next is None:
+            raise ValueError('p is no longer valid')
+        return p._node
+    
+    def _make_position(self, node):
+        if node is self._header or node is self._trailer:
+            return None
+        return self.Position(self, node)
+    
+    def first(self):
+        return self._make_position(self._header._next)
+    
+    def last(self):
+        return self._make_position(self._trailer._prev)
+    
+    def before(self, p):
+        node = self._validate(p)
+        return self._make_position(node._prev)
+    
+    def after(self, p):
+        node = self._validate(p)
+        return self._make_position(node._next)
+    
+    def __iter__(self):
+        cursor = self.first()
+        while cursor is not None:
+            yield cursor.element()
+            cursor = self.after(cursor)
+            
+    def _insert_between(self, e, predecessor, successor):
+        node = super()._insert_between(e, predecessor, successor)
+        return self._make_position(node)
+    
+    def add_first(self, e):
+        return self._insert_between(e, self._header, self._header._next)
+    
+    def add_last(self, e):
+        return self._insert_between(e, self._trailer._prev, self._trailer)
+    
+    def add_before(self, p, e):
+        original = self._validate(p)
+        return self._insert_between(e, original._prev, original)
+    
+    def add_after(self, p, e):
+        original = self._validate(p)
+        return self._insert_between(e, original, original._next)
+    
+    def delete(self, p):
+        original = self._validate(p)
+        return self._delete_node(original)
+    
+    def replace(self, p, e): #smart
+        original = self._validate(p)
+        old_value = original._element
+        original._element = e
+        return old_value
