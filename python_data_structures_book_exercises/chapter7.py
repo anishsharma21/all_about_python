6c1b28f06d1f543430e1c8623bd512b7efb44438
anishsharma21 <anisharma2103@gmail.com>
2024-07-07 12:06:43 +1000

    implemented a LinkedDeque class that inherits from the previously implemented _DoublyLinkedBase - this LinkedDeque inherits some methods so the super() call doesnt need to be made, but additional methods were added. The _insert_between and _delete_node from the super class are used for different methods in the LinkedDeque in a means to approach the implementation through an object-oriented manner

--- python_data_structures_book_exercises/chapter7.py
+++ python_data_structures_book_exercises/chapter7.py
@@ -141,7 +141,7 @@ class _DoublyLinkedBase:
         return self._size
     
     def is_empty(self):
-        return self.size == 0
+        return self._size == 0
     
     def _insert_between(self, e, predecessor, successor):
         new = _Node(e, predecessor, successor)
@@ -156,3 +156,31 @@ class _DoublyLinkedBase:
         e = node._element
         node._prev = node._next = node._element = None
         return e
+
+class LinkedDeque(_DoublyLinkedBase):
+    def first(self):
+        if self.is_empty():
+            raise Empty('queue is empty')
+        return self._header._next._element
+    
+    def last(self):
+        if self.is_empty():
+            raise Empty('queue is empty')
+        return self._trailer._prev._element
+    
+    def insert_first(self, e):
+        return self._insert_between(e, self._header, self._header._next)
+    
+    def insert_last(self, e):
+        return self._insert_between(e, self._trailer._prev, self._trailer)
+    
+    def delete_first(self):
+        if self.is_empty():
+            raise Empty('queue is empty')
+        return self._delete_node(self._header._next)
+    
+    def delete_last(self):
+        if self.is_empty():
+            raise Empty('queue is empty')
+        return self._delete_node(self._trailer._prev)
+        
