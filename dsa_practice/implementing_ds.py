b9f462e4c6cfa5eda9a43a6e645663d96f777a56
anishsharma21 <anisharma2103@gmail.com>
2024-07-08 20:01:07 +1000

    implementing a PositionalList that abstracts over the _DoublyLinkedBase class with a Position based interface

--- dsa_practice/implementing_ds.py
+++ dsa_practice/implementing_ds.py
@@ -33,3 +33,52 @@ class _DoublyLinkedBase:
         node._prev = node._next = node._element = None
         return element
 
+class PositionalList(_DoublyLinkedBase):
+    class Position:
+        def __init__(self, container, node):
+            self._container = container
+            self._node = node
+        
+        def element(self):
+            return self._node._element
+        
+        def __eq__(self, other):
+            return type(self) is type(other) and self._node is other._node
+        
+        def __ne__(self, other):
+            return not (self == other)
+        
+    def _validate(self, p):
+        if not isinstance(p, self.Position):
+            raise TypeError('not a valid Position object')
+        if p._container is not self:
+            raise ValueError('position does not belong to this PositionalList')
+        if p._node._next is None:
+            raise ValueError('position is no longer valid')
+        return p._node
+    
+    def _make_position(self, node):
+        if node is self._header or node is self._trailer:
+            return None
+        return self.Position(self, node)
+    
+    def first(self):
+        return _make_position(self._header._next)
+    
+    def last(self):
+        return _make_position(self._trailer._prev)
+    
+    def before(self, p):
+        node = self._validate(p)
+        return self._make_position(node._prev)
+    
+    def after(self, p):
+        node = self._validate(p)
+        return self._make_position(node._next)
+    
+    def __iter__(self):
+        cur = self.first()
+        while cur:
+            yield cur.element()
+            cur = seld.after(cur)
+
